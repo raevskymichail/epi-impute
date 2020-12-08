0ed99e9a1e4c46e26c1f6ab67a408fc388c8dce6
Mikhail Raevskiy <raevskymichail@gmail.com>
2020-12-08 18:31:46 +0300

    Debug & Add Documentation

--- epi.impute/R/epi-impute.R
+++ epi.impute/R/epi-impute.R
@@ -23,91 +23,91 @@
 #' 
 #' @export
 epi_impute <- function(sc_exp_data, sc_atac_data, sc_atac_cell_names,
-									sc_atac_peaks_ann, cell_types,
-									atac_bin_thrld = 100, ...){
-	sc_exp_data = as.data.frame(t(as.matrix(sc_exp_data))) 
-	sc_exp_data$gene = rownames(sc_exp_data)
+                                    sc_atac_peaks_ann, cell_types,
+                                    atac_bin_thrld = 100, ...) {
+  sc_exp_data <- as.data.frame(t(as.matrix(sc_exp_data)))
+  sc_exp_data$gene <- rownames(sc_exp_data)
 
-	promoters = sc_atac_peaks_ann[grepl('promoter|TSS', sc_atac_peaks_ann$X7),]
-	promoters_genes = promoters[order(promoters$X13),]
+  promoters <- sc_atac_peaks_ann[grepl('promoter|TSS', sc_atac_peaks_ann$X7),]
+  promoters_genes <- promoters[order(promoters$X13),]
 
-	sc_atac_data = merge(sc_atac_data, promoters_genes[c('X13', 'PeakFile_Peak_ID')], by = 'PeakFile_Peak_ID')
+  sc_atac_data <- merge(sc_atac_data, promoters_genes[c('X13', 'PeakFile_Peak_ID')], by = 'PeakFile_Peak_ID')
 
-	sc_atac_data <- setDT(sc_atac_data)
-	sc_atac_data = sc_atac_data[, lapply(.SD, sum), by = "X13", .SDcols = - "PeakFile_Peak_ID"]
-	sc_atac_genes = sc_atac_data$X13
-	sc_atac_data$X13 = NULL
+  sc_atac_data <- setDT(sc_atac_data)
+  sc_atac_data <- sc_atac_data[, lapply(.SD, sum), by = "X13", .SDcols = -"PeakFile_Peak_ID"]
+  sc_atac_genes <- sc_atac_data$X13
+  sc_atac_data$X13 <- NULL
 
-	genes_common = intersect(sc_atac_genes, sc_exp_data$gene)
-	sc_atac_data = sc_atac_data[sc_atac_genes %in% genes_common, ]
-	sc_atac_data = sc_atac_data[order(genes_common),]
+  genes_common <- intersect(sc_atac_genes, sc_exp_data$gene)
+  sc_atac_data <- sc_atac_data[sc_atac_genes %in% genes_common,]
+  sc_atac_data <- sc_atac_data[order(genes_common),]
 
-	sc_exp_data_genes = sc_exp_data[sc_exp_data$gene %in% genes_common, ]
-	sc_exp_data_genes = sc_exp_data_genes[order(genes_common),]
+  sc_exp_data_genes <- sc_exp_data[sc_exp_data$gene %in% genes_common,]
+  sc_exp_data_genes <- sc_exp_data_genes[order(genes_common),]
 
-	sc_atac_cell_names = sc_atac_cell_names[grepl(paste(cell_types, collapse="|"), sc_atac_cell_names$cell_types),]
-	sc_atac_data = as.data.frame(sc_atac_data) # next line will generate bool if comment this because of data.frame or use ", with=FALSE"
-	sc_atac_data = sc_atac_data[,colnames(sc_atac_data) %in% sc_atac_cell_names$cell_id]
-	sc_atac_cell_names = sc_atac_cell_names[sc_atac_cell_names$cell_id %in% colnames(sc_atac_data),]
+  sc_atac_cell_names <- sc_atac_cell_names[grepl(paste(cell_types, collapse = "|"), sc_atac_cell_names$cell_types),]
+  sc_atac_data <- as.data.frame(sc_atac_data) # next line will generate bool if comment this because of data.frame or use ", with=FALSE"
+  sc_atac_data <- sc_atac_data[, colnames(sc_atac_data) %in% sc_atac_cell_names$cell_id]
+  sc_atac_cell_names <- sc_atac_cell_names[sc_atac_cell_names$cell_id %in% colnames(sc_atac_data),]
 
-	celltype_list = split(sc_atac_cell_names, sc_atac_cell_names$cell_types)
-	celltype_list = celltype_list[cell_types] # there is no MLP
+  celltype_list <- split(sc_atac_cell_names, sc_atac_cell_names$cell_types)
+  celltype_list <- celltype_list[cell_types] # there is no MLP
 
-	bulk_peaks_list = list()
-	for(celltype in celltype_list){
-		bulk_peaks_df = rowSums(sc_atac_data[, celltype$cell_id])
-		bulk_peaks_df = as.data.frame(as.matrix((bulk_peaks_df > atac_bin_thrld) + 0))
-		bulk_peaks_list = list.append(bulk_peaks_list, bulk_peaks_df)
-	}
-	names(bulk_peaks_list) = cell_types
+  bulk_peaks_list <- list()
+  for (celltype in celltype_list) {
+    bulk_peaks_df <- rowSums(sc_atac_data[, celltype$cell_id])
+    bulk_peaks_df <- as.data.frame(as.matrix((bulk_peaks_df > atac_bin_thrld) + 0))
+    bulk_peaks_list <- list.append(bulk_peaks_list, bulk_peaks_df)
+  }
+  names(bulk_peaks_list) <- cell_types
 
-	print('Binirize the matrix')
-	sc_exp_data_genes_values = subset(sc_exp_data_genes, select = - gene)
-	sc_exp_data_genes_values = as.data.frame(as.matrix((sc_exp_data_genes_values > 0) + 0))
-	sc_exp_data_genes = cbind(sc_exp_data_genes$gene, sc_exp_data_genes_values)
-	colnames(sc_exp_data_genes)[1] = "gene"
+  print('Binirize the matrix')
+  sc_exp_data_genes_values <- subset(sc_exp_data_genes, select = -gene)
+  sc_exp_data_genes_values <- as.data.frame(as.matrix((sc_exp_data_genes_values > 0) + 0))
+  sc_exp_data_genes <- cbind(sc_exp_data_genes$gene, sc_exp_data_genes_values)
+  colnames(sc_exp_data_genes)[1] <- "gene"
 
-	sc_exp_data_genes = dcast(melt(sc_exp_data_genes, id.vars = "gene"), variable ~ gene)
-	rownames(sc_exp_data_genes) = sc_exp_data_genes$variable
-	sc_exp_data_genes$cells = gsub('_.*', '', sc_exp_data_genes$variable)
-	sc_exp_data_genes = sc_exp_data_genes[order(sc_exp_data_genes$cells),]
-	sc_exp_data_genes$variable = NULL
+  sc_exp_data_genes <- dcast(melt(sc_exp_data_genes, id.vars = "gene"), variable ~ gene)
+  rownames(sc_exp_data_genes) <- sc_exp_data_genes$variable
+  sc_exp_data_genes$cells <- gsub('_.*', '', sc_exp_data_genes$variable)
+  sc_exp_data_genes <- sc_exp_data_genes[order(sc_exp_data_genes$cells),]
+  sc_exp_data_genes$variable <- NULL
 
-	sc_data_list <- split(sc_exp_data_genes , f = sc_exp_data_genes$cells)
-	sc_data_list = lapply(sc_data_list, function(x){x$cells = NULL; as.data.frame(t(x))})
+  sc_data_list <- split(sc_exp_data_genes, f = sc_exp_data_genes$cells)
+  sc_data_list <- lapply(sc_data_list, function(x) { x$cells = NULL; as.data.frame(t(x)) })
 
-	print('Imputation...')
-	runtime = system.time({
-		celltypes_to_impute = cell_types
-		scRNAseq_imputed_list = list()
+  print('Imputation...')
+  runtime <- system.time({
+    celltypes_to_impute <- cell_types
+    scRNAseq_imputed_list <- list()
 
-		for(i in celltypes_to_impute){
-			imputed_i = as.data.frame(sapply(as.data.frame(sc_data_list[[i]]), function(x){x + bulk_peaks_list[[i]]}))
-			scRNAseq_imputed_list = list.append(scRNAseq_imputed_list, as.data.frame(imputed_i))
-		}
+    for (i in celltypes_to_impute) {
+      imputed_i <- as.data.frame(sapply(as.data.frame(sc_data_list[[i]]), function(x) { x + bulk_peaks_list[[i]] }))
+      scRNAseq_imputed_list <- list.append(scRNAseq_imputed_list, as.data.frame(imputed_i))
+    }
 
-		scRNAseq_imputed_list = as.data.frame(scRNAseq_imputed_list)
-		rownames(scRNAseq_imputed_list) = genes_common
-	})
+    scRNAseq_imputed_list <- as.data.frame(scRNAseq_imputed_list)
+    rownames(scRNAseq_imputed_list) <- genes_common
+  })
 
-	return(as.data.frame(t(scRNAseq_imputed_list))
+  return(as.data.frame(t(scRNAseq_imputed_list)))
 }
 
 #' Loads example data for imputation
 #'
 #' @export
 load_example_data <- function() {
-	library(RCurl)
-	library(readr)
-	library(feather)
+  library(RCurl)
+  library(readr)
+  library(feather)
 
-	sc_atac_cell_names <- read_csv(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_cell_names_matrix.csv"))
-	sc_atac_data <- read_feather(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_scATACseq_matrix.feather"))
-	sc_atac_peaks_ann <- read_csv(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_PeakFile.csv"))
-	sc_exp_data <- get(load(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE117498_scRNAseq_genes.Rdata")))
+  sc_atac_cell_names <- read_csv(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_cell_names_matrix.csv"))
+  sc_atac_data <- read_feather(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_scATACseq_matrix.feather"))
+  sc_atac_peaks_ann <- read_csv(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE96769_PeakFile.csv"))
+  sc_exp_data <- get(load(url("https://raw.github.com/raevskymichail/epi-impute/master/data/GSE117498_scRNAseq_genes.Rdata")))
 
-	return(list(sc_atac_cell_names = sc_atac_cell_names,
-				sc_atac_data = sc_atac_data
-				sc_atac_peaks_ann = sc_atac_peaks_ann
-				sc_exp_data = sc_exp_data))
+  return(list(sc_atac_cell_names = sc_atac_cell_names,
+                    sc_atac_data = sc_atac_data,
+                    sc_atac_peaks_ann = sc_atac_peaks_ann,
+                    sc_exp_data = sc_exp_data))
 }
